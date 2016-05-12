1748fde82a34f2d2124894235e711f01d7b685b3
Armen Zambrano G <armenzg@mozilla.com>
2016-05-12 12:06:13 -0400

    Initial release

--- schedule_linux64_task.py
+++ schedule_linux64_task.py
@@ -1,12 +1,13 @@
+import ast
 import json
 import logging
 import os
-import webbrowser
 
-from mozci.taskcluster import TaskClusterManager, get_task_inspector_url
+import taskcluster as taskcluster_client
+from mozci import TaskClusterManager
 from mozci.utils.log_util import setup_logging
 
-setup_logging(logging.INFO)
+setup_logging(logging.DEBUG)
 credentials = None
 
 try:
@@ -17,8 +18,7 @@ except IOError:
     tc = TaskClusterManager(web_auth=True)
 
 
-with open(os.path.join('artifacts', 'hello_world_task.json')) as file:
+with open(os.path.join('artifacts', 'linux64_test_task.json')) as file:
     task = json.load(file)
 
-task = tc.schedule_task(task=task)
-webbrowser.open_new_tab(url=get_task_inspector_url(task['status']['taskId']))
+tc.schedule_task(task=task)
