1f87a0866c628b717db1645b6d3ea98930a8c667
Armen Zambrano <armenzg@users.noreply.github.com>
2016-05-17 16:53:43 -0400

    Script to schedule a Linux64 task (#1)
    This script allows you to schedule a sample Linux64 debug test job with your TaskCluster temporary credentials.
    
    This scheduling has some limitation like the job will not show up on Treeherder and will not make changes to the index.

--- schedule_linux64_task.py
+++ schedule_linux64_task.py
@@ -1,9 +1,10 @@
+import ast
 import json
 import logging
 import os
-import webbrowser
 
-from mozci.taskcluster import TaskClusterManager, get_task_inspector_url
+import taskcluster as taskcluster_client
+from mozci import TaskClusterManager
 from mozci.utils.log_util import setup_logging
 
 setup_logging(logging.INFO)
@@ -17,8 +18,10 @@ except IOError:
     tc = TaskClusterManager(web_auth=True)
 
 
-with open(os.path.join('artifacts', 'hello_world_task.json')) as file:
+with open(os.path.join('artifacts', 'linux64_test_task.json')) as file:
     task = json.load(file)
 
-task = tc.schedule_task(task=task)
-webbrowser.open_new_tab(url=get_task_inspector_url(task['status']['taskId']))
+# https://groups.google.com/d/msg/mozilla.tools.taskcluster/nkzBlX7BgrU/-IrWXPXCAQAJ
+# This prevents us from scheduling a task which would show up on Treeherder
+task['routes'] = []
+tc.schedule_task(task=task)
